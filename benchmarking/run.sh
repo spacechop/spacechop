64f482cb46575745b0596a8736df07b18a7b8534
Eric Wennerberg <ericwenn@gmail.com>
2018-08-20 19:17:42 -0700

    Add more images for benchmarking

--- benchmarking/run.sh
+++ benchmarking/run.sh
@@ -1,14 +1,22 @@
 # /f/fd/ANGELO_Saison_1.png
 # /e/e8/Kalidasa_lanata-Kadavoor-2017-05-23-003.jpg
-docker pull williamyeh/wrk
-docker pull nginx
+docker pull williamyeh/wrk > /dev/null
+docker pull nginx > /dev/null
+
+docker network create sc-benchmark
 
 # Start container and store hash of container
-sc_hash=$(docker run -d -p 8080:3000 -v $(pwd)/benchmarking-config.yml:/config.yml spacechop node dist/index.js)
-nginx_hash=$(docker run -d -p 8081:80 -v $(pwd)/assets:/usr/share/nginx/html nginx)
-sleep 2
-docker run --rm --net=host williamyeh/wrk -t 20 -c 40 -d 30 http://localhost:8080/t_720/wallet.jpg
+sc_hash=$(docker run --net=sc-benchmark --name spacechop-benched -d -v $(pwd)/benchmarking-config.yml:/config.yml spacechop node dist/index.js)
+nginx_hash=$(docker run --net=sc-benchmark --name nginx -d -v $(pwd)/assets:/usr/share/nginx/html nginx)
+sleep 5
 
-# docker cp 
-docker kill ${sc_hash}
-docker kill ${nginx_hash}
+for image in $(ls ./assets)
+do
+  echo "Running ${image}..."
+  docker run --rm --net=sc-benchmark williamyeh/wrk -t 4 -c 10 -d 5 http://spacechop-benched:3000/t_720/${image}
+  echo ""
+done
+
+docker kill ${sc_hash} ${nginx_hash} > /dev/null
+docker rm ${sc_hash} ${nginx_hash} > /dev/null
+docker network rm sc-benchmark > /dev/null
