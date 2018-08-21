141f3902eaa02d15d0a2d3f42b9ed16460a2dd0d
Tim Brandin <tim.brandin@gmail.com>
2018-08-21 14:11:36 -0700

    Fixed faulty fill type.

--- src/operations/fill/types.ts
+++ src/operations/fill/types.ts
@@ -7,12 +7,7 @@ const FillConfig = t.Record({
   height: PostiveNumber,
 }).And(t.Partial({
   gravity: Gravity,
-})).withConstraint((n) => {
-  if (!(n.width || n.height)) {
-    return 'missing width or height';
-  }
-  return true;
-});
+}));
 
 export type FillConfig = t.Static<typeof FillConfig>;
 export default FillConfig;
