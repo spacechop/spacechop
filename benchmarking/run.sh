ce1c21d5c8f93503ce5ddc157e4def816d61ada1
Eric Wennerberg <ericwenn@gmail.com>
2018-08-20 18:31:57 -0700

    remove prof flag

--- benchmarking/run.sh
+++ benchmarking/run.sh
@@ -4,7 +4,7 @@ docker pull williamyeh/wrk
 docker pull nginx
 
 # Start container and store hash of container
-sc_hash=$(docker run -d -p 8080:3000 -v $(pwd)/benchmarking-config.yml:/config.yml spacechop node --prof dist/index.js)
+sc_hash=$(docker run -d -p 8080:3000 -v $(pwd)/benchmarking-config.yml:/config.yml spacechop node dist/index.js)
 nginx_hash=$(docker run -d -p 8081:80 -v $(pwd)/assets:/usr/share/nginx/html nginx)
 sleep 2
 docker run --rm --net=host williamyeh/wrk -t 20 -c 40 -d 30 http://localhost:8080/t_720/wallet.jpg
