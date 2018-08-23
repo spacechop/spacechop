91ba4f1aca5d1dc805c2b3feb2e75386af354a7a
Tim Brandin <tim.brandin@gmail.com>
2018-08-23 12:49:14 +0200

    Added tests for config load.

--- src/config/load.ts
+++ src/config/load.ts
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
+import console from '../lib/console';
 
 export const loadConfig = (filepath) => yaml.safeLoad(fs.readFileSync(filepath));
 
 export default (filepath) => {
-  if (fs.existsSync('/config.yml')) {
+  if (fs.existsSync(filepath)) {
     try {
-      return loadConfig(filepath);
+      return loadConfig(filepath) || null;
     } catch (err) {
       console.error(`Could not load ${filepath}`);
       console.error(err);
