51dda5cc4b80634a70d05a6dc331d302ce9bd0bb
Tim Brandin <tim.brandin@gmail.com>
2018-08-23 18:15:29 +0200

    Improving test for extract stream data.

--- src/lib/__tests__/extractStreamMeta.ts
+++ src/lib/__tests__/extractStreamMeta.ts
@@ -4,7 +4,7 @@ import extractStreamMeta from '../extractStreamMeta';
 
 describe('Extract stream meta', () => {
   const assetsFolder = path.join(__dirname, './../../test/assets');
-  const filename = path.join(assetsFolder, 'cat.jpg');
+  const filename = path.join(assetsFolder, 'grid.jpg');
   const stream = fs.createReadStream(filename);
 
   it('should extract meta data from image', async () => {
@@ -16,7 +16,16 @@ describe('Extract stream meta', () => {
     delete meta.elapsedTime;
     delete meta.pixelsPerSecond;
     delete meta.userTime;
-    expect(meta).toMatchSnapshot();
+    expect(meta).toEqual(
+      expect.objectContaining({
+        mimeType: 'image/jpeg',
+        interlace: 'None',
+        geometry: expect.objectContaining({
+          width: 100,
+          height: 100,
+        }),
+      }),
+    );
   });
 
   it('should throw when stream is broken', async () => {
