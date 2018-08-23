1b1b7b09b5fa4945fe94aeac0f9ce6604e905f35
Tim Brandin <tim.brandin@gmail.com>
2018-08-23 14:57:21 +0200

    Added tests for config load and validate and improved load tests.

--- src/config/__tests__/index.ts
+++ src/config/__tests__/index.ts
@@ -1,30 +1,32 @@
 import path from 'path';
-import load from '../load';
+import load from '../index';
 
 jest.mock('../../lib/console');
 
 describe('should load config.yml', () => {
   describe('invalid config', () => {
-    it('should throw on missing file', () => {
+    it('should return null missing file', () => {
       const filename = path.join(__dirname, 'assets/config.yml');
-      expect(() => load(filename)).toThrowErrorMatchingSnapshot();
+      expect(load(filename)).toBe(null);
     });
 
-    it('should throw on empty', () => {
+    it('should return null on empty', () => {
       const filename = path.join(__dirname, 'assets/empty.yml');
-      expect(() => load(filename)).toThrowErrorMatchingSnapshot();
+      expect(load(filename)).toBe(null);
     });
 
-    it('should throw on bad yaml', () => {
+    it('should return null on bad yaml', () => {
       const filename = path.join(__dirname, 'assets/bad.yml');
-      expect(() => load(filename)).toThrowErrorMatchingSnapshot();
+      expect(load(filename)).toBe(null);
     });
   });
 
   describe('valid config', () => {
     it('should return config', () => {
       const filename = path.join(__dirname, 'assets/valid.yml');
-      expect(load(filename)).toMatchSnapshot();
+      const config = load(filename);
+      expect(config).not.toBe(null);
+      expect(config).toMatchSnapshot();
     });
   });
 });
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,11 @@
-import * as t from 'runtypes';
-import Config from '../types/Config';
+import console from '../lib/console';
+import { Config } from '../types/Config';
 import load from './load';
 import validate from './validate';
-import console from '../lib/console';
 
-export default (): t.Static<typeof Config> => {
-  const config = load('/config.yml');
+export default (filename = '/config.yml'): Config => {
   try {
+    const config = load(filename);
     if (config && validate(config)) {
       return config;
     }
