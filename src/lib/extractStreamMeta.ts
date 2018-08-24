5f30021e07150db0be4ddbbed505523ce3da2274
Tim Brandin <tim.brandin@gmail.com>
2018-08-24 07:47:40 +0200

    Only replace when broken json.

--- src/lib/extractStreamMeta.ts
+++ src/lib/extractStreamMeta.ts
@@ -15,7 +15,7 @@ export default async (stream: Stream): Promise<any> => new Promise((resolve, rej
     // replace -nan with null
     data = data.replace(/\-nan/g, 'null');
     // replace originGeometry with string
-    data = data.replace(/"originGeometry": ([^,]+),/g, '"originGeometry": "$1",');
+    data = data.replace(/"originGeometry": ([^,"]+),/g, '"originGeometry": "$1",');
     const json = JSON.parse(data);
     const [{ image }] = json;
     resolve(image);
