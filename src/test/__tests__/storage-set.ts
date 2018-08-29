8a33da6cff337da35c0188a3df3ee0390fdb8ba8
Eric Wennerberg <ericwenn@gmail.com>
2018-08-29 10:07:05 +0200

    Test that image is uploaded to storage

--- src/test/__tests__/storage-set.ts
+++ src/test/__tests__/storage-set.ts
@@ -5,38 +5,37 @@ import path from 'path';
 import pathToRegex from 'path-to-regexp';
 import { Request, Response } from './../utils/expressMocks';
 import { PassThrough } from 'stream';
+import { createReadStream } from 'fs';
 
 /**
  * Tests in this file use the full spacechop implementation by mocking Express 
  * Response and Requests.
  */
 describe('Configured storage', () => {
-  
-  describe('Image exists in storage cache', () => {
-    const p = '/:preset/:image'
-    const config: Config = {
-      sources: [{
-        volume: {
-          root: path.join(assetsFolder, ':image')
-        }
-      }],
-      paths: [p],
-      presets: {
-        t_original: {
-          steps: []
-        }
-      },
-      storage: {
-        s3: {
-          access_key_id: 'xxx',
-          bucket_name: 'yy',
-          path: '',
-          region: 'nyc2',
-          secret_access_key: 'zz'
-        }
+  const p = '/:preset/:image'
+  const config: Config = {
+    sources: [{
+      volume: {
+        root: path.join(assetsFolder, ':image')
       }
-    };
-  
+    }],
+    paths: [p],
+    presets: {
+      t_original: {
+        steps: []
+      }
+    },
+    storage: {
+      s3: {
+        access_key_id: 'xxx',
+        bucket_name: 'yy',
+        path: '',
+        region: 'nyc2',
+        secret_access_key: 'zz'
+      }
+    }
+  };
+  describe('Image exists in storage cache', () => {
     const sources = [
       {
         exists: jest.fn(),
@@ -77,6 +76,48 @@ describe('Configured storage', () => {
     });
     it('should not call storage .upload', () => {
       expect(storage.upload).not.toHaveBeenCalled();
+    });
+  })
+
+
+  describe('Image does not exists in storage cache', () => {
+    const image = path.join(assetsFolder, 'grid.png')
+
+    const sources = [
+      {
+        exists: jest.fn(() => Promise.resolve(true)),
+        stream: jest.fn(() => Promise.resolve(createReadStream(image))),
+      }
+    ];
+    const storage = {
+      exists: jest.fn(() => Promise.resolve(false)),
+      stream: jest.fn(),
+      upload: jest.fn(),
+    };
+
+    const keys = [];
+    // populates `keys` array
+    pathToRegex(p, keys);
+    const handler = requestHandler(config, keys, sources, storage);
+
+    let request;
+    let response;
+    beforeAll(async () => {
+      request = new Request();
+      response = new Response();
+      request.setParams(0, 't_original');
+      request.setParams(1, 'grid.png');
+      await handler(request, response);
+    })
+    it('should check storage', () => {
+      expect(storage.exists).toHaveBeenCalled();
+    });
+    it('should check source', () => {
+      expect(sources[0].exists).toHaveBeenCalled();
+      expect(sources[0].stream).toHaveBeenCalled();
+    });
+    it('should call storage .upload', () => {
+      expect(storage.upload).toHaveBeenCalled();
     });
   })
 })
