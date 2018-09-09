c2aec582c3039edd481f061336b334df8c226a08
Tim Brandin <tim.brandin@gmail.com>
2018-09-09 17:18:00 +0200

    Cleaning up.

--- src/imagedef/analyze.ts
+++ src/imagedef/analyze.ts
@@ -3,25 +3,4 @@ import analyzeStream from '../lib/analyzeStream';
 
 export default async (stream, requirements): Promise<ImageDefinition> => {
   return analyzeStream(stream, requirements);
-  // const {
-  //   mimeType,
-  //   geometry: {
-  //     width,
-  //     height,
-  //   },
-  //   interlace,
-  //   channelDepth,
-  //   alpha,
-  //   animatedGIF,
-  //   animatedPNG,
-  // } = data;
-  //
-  // return {
-  //   width,
-  //   height,
-  //   alpha: channelDepth && !!channelDepth.alpha || !!alpha,
-  //   interlacing: interlace !== 'None',
-  //   type,
-  //   animated: animatedGIF || animatedPNG,
-  // };
 };
