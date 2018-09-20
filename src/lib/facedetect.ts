f8d3f9dde911999141d68da5e0ef4341c5bdf53d
Tim Brandin <tim.brandin@gmail.com>
2018-09-21 00:45:16 +0200

    Added missing file.

---
+++ src/lib/facedetect.ts
@@ -0,0 +1,43 @@
+import { spawn } from 'duplex-child-process';
+import { Stream } from 'stream';
+import uuid from 'uuid/v1';
+import ImageDefinition, { ImageFaceBox } from '../imagedef';
+
+export const scaleFace = (
+  scale: number,
+) => (face: ImageFaceBox): ImageFaceBox => {
+  return {
+    x: Math.round(face.x / scale),
+    y: Math.round(face.y / scale),
+    width: Math.round(face.width / scale),
+    height: Math.round(face.height / scale),
+  };
+};
+
+export default (
+  streamToAnalyze: Stream,
+  info: ImageDefinition,
+): Promise<ImageFaceBox[]> => new Promise((resolve, reject) => {
+  const filepath = `/tmp/${uuid()}.jpg`;
+  const command = [
+    `magick - -resize 1000x1000\\> ${filepath}`,
+    `facedetect ${filepath} -rm`,
+  ].join('&&');
+  const stream = spawn('sh', ['-c', command]);
+  streamToAnalyze.pipe(stream);
+  const chunks = [];
+  stream.on('data', (chunk) => {
+    chunks.push(chunk);
+  });
+  stream.on('error', (err) => reject(err));
+  stream.on('end', () => {
+    const buffer = Buffer.concat(chunks).toString('utf8');
+    try {
+      const data = JSON.parse(buffer);
+      const scale = Math.min(1, info.width > info.height ? 1000 / info.width : 1000 / info.height);
+      resolve(data.map(({ face }) => scaleFace(scale)(face)));
+    } catch (err) {
+      reject(err);
+    }
+  });
+});
