8aaead3e30f9c4439206531dccfbc465d28667ba
Eric Wennerberg <ericwenn@gmail.com>
2018-09-20 20:08:38 +0200

    Fix issue with analyzing cached images

--- src/spacechop.ts
+++ src/spacechop.ts
@@ -45,6 +45,12 @@ export const requestHandler = (
   // check if transformation is already done and exists in storage
   if (storage) {
     const fromCache = await fetchFromStorage(storage, params);
+
+    if ('analyze' in req.query) {
+      const { state } = await buildTransformation(fromCache.stream, []);
+      res.json(state);
+      return;
+    }
     // It exists in cache
     if (fromCache && fromCache.contentType) {
       res.set('Content-Type', fromCache.contentType);
