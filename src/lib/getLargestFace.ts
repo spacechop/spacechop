731e703aba60a04a27112e2b1cfca804363bc741
Tim Brandin <tim.brandin@gmail.com>
2018-09-25 21:09:22 +0200

    Added test on getLargestFace

--- src/lib/getLargestFace.ts
+++ src/lib/getLargestFace.ts
@@ -3,12 +3,15 @@ import { ImageFaceBox } from '../imagedef';
 export default (faces: ImageFaceBox[]): ImageFaceBox => {
   if (faces && faces.length > 0) {
     // Find the largest face.
-    let face = faces[0];
-    faces.forEach(({ width, height }, i) => {
-      if (face.width * face.height > width * height) {
+    let face;
+    let max = 0;
+    for (let i = 0; i <= faces.length - 1; i++) {
+      const area = faces[i].width * faces[i].height;
+      if (area > max) {
+        max = area;
         face = faces[i];
       }
-    });
+    }
     return face;
   }
   return null;
