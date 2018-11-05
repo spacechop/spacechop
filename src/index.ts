6c729ce1ed805283cd981a6ecd25236a177c6392
Tim Brandin <tim.brandin@gmail.com>
2018-11-05 20:55:46 +0100

    Removing cluster that only increases complexity.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import chokidar from 'chokidar';
-import cluster from 'cluster';
 import express from 'express';
-import os from 'os';
 import loadConfig from './config';
 import monitor from './monitor';
 import setupRoutes from './spacechop';
@@ -30,36 +28,20 @@ app.use((req, res, next) => {
   router(req, res, next);
 });
 
-if (cluster.isMaster) {
-  const workers = os.cpus().length;
-  for (let i = 0; i < workers; i++) {
-    cluster.fork();
-  }
+// Re-Initialize routes when new config is loaded.
+chokidar.watch('/config.yml', {
+  usePolling: true,
+  interval: 1000,
+}).on('all', async () => {
+  console.info('Reloading config...');
+  router = express.Router();
+  config = loadConfig();
+  setupRoutes(config, router, monitor);
+});
 
-  // Catching errors and creating new workers.
-  cluster.on('exit', (worker, code, signal) => {
-    console.info(
-      'worker %d died (%s). restarting...',
-      worker.process.pid, signal || code,
-    );
-    cluster.fork();
-  });
-} else {
-  // Re-Initialize routes when new config is loaded.
-  chokidar.watch('/config.yml', {
-    usePolling: true,
-    interval: 1000,
-  }).on('all', async () => {
-    console.info('Reloading config...');
-    router = express.Router();
-    config = loadConfig();
-    setupRoutes(config, router, monitor);
-  });
+app.get(PROMETHEUS_METRIC_PATH, (_, res) => {
+  res.end(monitor.getMetrics());
+});
 
-  app.get(PROMETHEUS_METRIC_PATH, (_, res) => {
-    res.end(monitor.getMetrics());
-  });
-
-  // start listening on port.
-  server = app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
-}
+// start listening on port.
+server = app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
